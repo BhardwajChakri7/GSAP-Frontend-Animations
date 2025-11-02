03d0aa52bf99fd6d7570ebe725f466e27a2467cb
Bhardwaj Chakri <bhardwajchakri@gmail.com>
2025-11-02 21:55:56 +0530

    OneBoxes Animation base Code

---
+++ OneBoxesAnimation/src/App.jsx
@@ -0,0 +1,9 @@
+import "./index.css";
+const App = () => {
+  return (
+    <main className="h-screen w-full bg-black p-20">
+      <div className="h-60 w-60 bg-red-900 rounded-2xl p-20">Hello</div>
+    </main>
+  );
+};
+export default App;
