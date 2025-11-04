fa950b81e54d16e28078cece617bfc985a2a5342
Bhardwaj Chakri <bhardwajchakri@gmail.com>
2025-11-05 00:43:30 +0530

    Final Code For Timeline Box Animation

--- Gsap4TimelineBoxAnimation/src/App.jsx
+++ Gsap4TimelineBoxAnimation/src/App.jsx
@@ -1,7 +1,39 @@
-import { React } from "react";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import "./index.css";
 
 const App = () => {
-  return <main>Hello</main>;
+  const tl = gsap.timeline();
+
+  const boxRef1 = useRef();
+  const boxRef2 = useRef();
+  const boxRef3 = useRef();
+  useGSAP(() => {
+    tl.to(boxRef1.current, {
+      x: 1000,
+      duration: 2,
+      delay: 1,
+      rotate: 360,
+    });
+    tl.to(boxRef2.current, {
+      x: 1000,
+      duration: 2,
+      rotate: 1080,
+      borderRadius: "40%",
+    });
+    tl.to(boxRef3.current, {
+      x: 1000,
+      duration: 2,
+    });
+  });
+
+  return (
+    <main className="bg-black h-screen w-full p-10">
+      <div ref={boxRef1} className="bg-red-400 h-50 w-50 m-3"></div>
+      <div ref={boxRef2} className="bg-red-600 h-50 w-50 m-3"></div>
+      <div ref={boxRef3} className="bg-red-800 h-50 w-50 m-3"></div>
+    </main>
+  );
 };
 export default App;
