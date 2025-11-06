fe829e988f642c0458999e9537649f5c2e2fe6fb
Bhardwaj Chakri <bhardwajchakri@gmail.com>
2025-11-06 16:40:54 +0530

    Final Code For TextSliding Animation

--- Gsap7TextSlidingAnimation/src/App.jsx
+++ Gsap7TextSlidingAnimation/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import "./index.css";
+gsap.registerPlugin(ScrollTrigger);
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const mainContainerRef = useRef();
+  const mainHeadingRef = useRef();
+  useGSAP(() => {
+    gsap.to(mainHeadingRef.current, {
+      xPercent: -62,
+      yoyo: true,
+      scrollTrigger: {
+        trigger: mainContainerRef.current,
+        scroller: "body",
+        markers: true,
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+        pin: true,
+      },
+    });
+  });
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen w-full bg-blue-500 flex flex-row items-center justify-center">
+        <h1 className="text-5xl font-bold">Scroll To Know the Animation</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div
+        ref={mainContainerRef}
+        className="h-screen w-full bg-black text-center flex items-center">
+        <h1 ref={mainHeadingRef} className="text-[40vw] font-bold uppercase">
+          Experience
+        </h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="h-screen w-full bg-blue-500"></div>
     </>
-  )
-}
-
-export default App
+  );
+};
+export default App;
