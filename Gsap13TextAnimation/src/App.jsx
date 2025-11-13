d17e3f78dfbcc16f6eec26c020fb14cea31b1b1b
Bhardwaj Chakri <bhardwajchakri@gmail.com>
2025-11-13 22:09:00 +0530

    Initial Code For Text Animation

---
+++ Gsap13TextAnimation/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <main>
+      <h1>Hello World!!!</h1>
+    </main>
+  );
+};
+export default App;
