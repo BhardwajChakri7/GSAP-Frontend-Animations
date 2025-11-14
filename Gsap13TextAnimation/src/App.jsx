01dbe0fc5aefacf56c1441677032dcd8e2b3e8e2
Bhardwaj Chakri <bhardwajchakri@gmail.com>
2025-11-14 21:58:04 +0530

    Mid Range Completion of Task

--- Gsap13TextAnimation/src/App.jsx
+++ Gsap13TextAnimation/src/App.jsx
@@ -4,10 +4,27 @@ import { useGSAP } from "@gsap/react";
 import "./index.css";
 
 const App = () => {
+  const mainTextRef = useRef();
+  useGSAP(() => {
+    var h1Text = mainTextRef.current.textContent;
+    var letters = h1Text.split("");
+    var cutter = "";
+    letters.forEach((element) => {
+      cutter += `<span className="inline-block">${element}</span>`;
+    });
+    mainTextRef.current.innerHTML = cutter;
+    gsap.from(mainTextRef.current.querySelectorAll("span"), {
+      y: 100,
+      duration: 0.5,
+      stagger: 0.3,
+    });
+  });
   return (
-    <main>
-      <h1>Hello World!!!</h1>
-    </main>
+    <div className="h-screen w-full bg-black flex justify-center items-center">
+      <h1 ref={mainTextRef} className="text-6xl font-extrabold">
+        BHARDWAJCHAKRI
+      </h1>
+    </div>
   );
 };
 export default App;
